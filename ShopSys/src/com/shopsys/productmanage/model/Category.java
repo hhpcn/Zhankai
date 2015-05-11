d76368cc41ba5fc495c3776f873b8b269250d402
lk <lk@qq.com>
2015-05-11 23:46:55 +0800

    2015-05-11 lk 首页调整

---
+++ ShopSys/src/com/shopsys/productmanage/model/Category.java
@@ -0,0 +1,13 @@
+package com.shopsys.productmanage.model;
+
+public class Category implements java.io.Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1207976358200883001L;
+	private Integer id;
+	private String categoryName;
+	private String productName;
+	private String detailInfo;
+}
