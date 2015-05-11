324b2db1a375ae07dfff0adff8ff9b24a1f1eb0b
lk <lk@qq.com>
2015-05-12 01:08:32 +0800

    2015-05-11 hhp 栏目管理

---
+++ ShopSys/src/com/shopsys/productmanage/action/CategoryAction.java
@@ -0,0 +1,124 @@
+package com.shopsys.productmanage.action;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.shopsys.common.JqgridUtil;
+import com.shopsys.personnel.model.User;
+import com.shopsys.productmanage.model.Category;
+import com.shopsys.productmanage.service.CategoryService;
+import com.xmut.base.BaseAction;
+
+public class CategoryAction extends BaseAction {
+
+	private static final long serialVersionUID = 1L;
+	private CategoryService categoryService;
+	private Category category;
+	
+	private String  categoryName;
+	private String pageUrl;
+	
+	
+	
+	/**
+	 * 查询用户数据，包括条件分页搜索和非条件分页搜索两种
+	 * @return
+	 */
+	public String listCategory() {
+		dataMap=new HashMap<String, Object>();
+		int currentPage=Integer.parseInt(page);
+		int pageSize=Integer.parseInt(rows);
+		List<Category> categories =null;
+		List<Map<String, Object>> categoryMaps = new ArrayList<Map<String, Object>>();
+		int allRecordNumbers=0;
+		
+		if (_search) {
+			String whereParams = JqgridUtil.SearchToHqlParam(null, null, null, filters, null, null, null);
+			categories=categoryService.listPageRowsByClassAndParams(Category.class, currentPage, pageSize, whereParams);
+			allRecordNumbers=categoryService.countByClassAndParams(Category.class, whereParams);
+			dataMap.put("total", JqgridUtil.countPageNumbers(pageSize, allRecordNumbers));//总页数
+			dataMap.put("records", allRecordNumbers);//总记录数
+		}else {
+			allRecordNumbers=categoryService.countByClass(Category.class);
+		    categories=categoryService.listPageRowsByClass(Category.class, currentPage, pageSize);
+			dataMap.put("total", JqgridUtil.countPageNumbers(pageSize, allRecordNumbers));//总页数
+			dataMap.put("records", allRecordNumbers);//总记录数
+		}
+		for (Category category : categories) {
+			Map<String, Object> categoryMap= new HashMap<String, Object>();
+			categoryMap.put("categoryName", category.getCategoryName());
+			categoryMap.put("id", category.getId());
+			categoryMap.put("pageUrl", category.getPageUrl());
+			categoryMaps.add(categoryMap);
+		}
+	
+		dataMap.put("rows", categoryMaps);
+		
+		return "list";
+	}
+	
+	//行编辑：更新，删除
+	public String edit() {
+		if("add".equalsIgnoreCase(oper)){
+			Category category = new Category();
+			category.setCategoryName(categoryName);
+			category.setPageUrl(pageUrl);
+			categoryService.saveOrUpdate(Category.class, category);
+			flag=true;
+		}else{
+			Integer[] intIds = JqgridUtil.idToIntIds(id);
+			if ("edit".equalsIgnoreCase(oper)) {
+				//在某一时刻编辑时只有一行记录能被编辑，浏览器发送过看来的ids只有一个id，所以这里直接去intIds[0]
+				Category categoryData= categoryService.getByClassNameAndId(Category.class, intIds[0]);
+				categoryData.setCategoryName(categoryName);
+				categoryData.setPageUrl(pageUrl);
+				categoryService.update(categoryData);
+				flag=true;
+				
+			}else if ("del".equalsIgnoreCase(oper)) {
+				categoryService.deleteByClassAndIds(Category.class, intIds);
+				flag=true;
+			}
+		}
+		
+		return "flag";
+	}
+	
+	
+	
+	
+	
+	public CategoryService getCategoryService() {
+		return categoryService;
+	}
+	public void setCategoryService(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getPageUrl() {
+		return pageUrl;
+	}
+
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+	
+	
+	
+}
---
+++ ShopSys/src/com/shopsys/productmanage/serviceimpl/CategoryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.shopsys.productmanage.serviceimpl;
+
+import com.shopsys.productmanage.service.CategoryService;
+import com.xmut.base.BaseServiceImpl;
+
+public class CategoryServiceImpl extends BaseServiceImpl implements
+		CategoryService {
+
+}
