2dd47a151071d81138d3c4c53f998f030d88041f
hhp <hhp@59.77.132.71>
2015-05-15 17:22:49 +0800

    2015-05-15 hhp实验室 导航栏js脚本修改

--- ShopSys/WebRoot/js/front/shouyenav.js
+++ ShopSys/WebRoot/js/front/shouyenav.js
@@ -1,20 +1,29 @@
 jQuery(document).ready(function(){
 		var qcloud={};
-		$('[_t_nav]').hover(function(){
+		$('[_t_nav]').hover(
+		function(){
 			var _nav = $(this).attr('_t_nav');
 			clearTimeout( qcloud[ _nav + '_timer' ] );
 			qcloud[ _nav + '_timer' ] = setTimeout(function(){
-			$('[_t_nav]').each(function(){
-			$(this)[ _nav == $(this).attr('_t_nav') ? 'addClass':'removeClass' ]('nav-up-selected');
-			});
-			$('#'+_nav).stop(true,true).slideDown(200);
+				$('[_t_nav]').each(function(){
+					$(this)[ _nav == $(this).attr('_t_nav') ? 'addClass':'removeClass' ]('nav-up-selected');
+				});
+				$('#'+_nav).slideDown(200);
 			}, 150);
 		},function(){
 			var _nav = $(this).attr('_t_nav');
 			clearTimeout( qcloud[ _nav + '_timer' ] );
 			qcloud[ _nav + '_timer' ] = setTimeout(function(){
 			$('[_t_nav]').removeClass('nav-up-selected');
-			$('#'+_nav).stop(true,true).slideUp(200);
+			$('#'+_nav).slideUp(200);
 			}, 150);
 		});
+		
+	
+		$('[_t_nav]').click(function(){
+			var _nav = $(this).attr('_t_nav');
+			$('[_t_nav]').removeClass('nav-up-selected-inpage');
+			$("[_t_nav='"+_nav+"']").addClass('nav-up-selected-inpage');
+		});
+				
 });
