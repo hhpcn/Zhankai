92be657522d2b42438bb2a4c8eaca404b8f9ac4c
lk <lk@qq.com>
2015-05-24 23:47:38 +0800

    5.24.lk.proDetails

--- ShopSys/WebRoot/js/front/productsDetail.js
+++ ShopSys/WebRoot/js/front/productsDetail.js
@@ -44,17 +44,57 @@ $(function(){
 			/*
 			var productList=data.rows;
 			var productHtml="";
-			
 			productHtml="<div class='col-md-12'><div class='row' >"+productHtml+"</div></div>";
 			
 			$("#productlist").empty().html(productHtml);*/
 			
 			document.getElementById("productDis").innerHTML=
-			"<div style='margin-left: 10px;font-size: 14px;margin-top: 10px;margin-right: 10px'>产品名称："+data.product.productName+"</div>"+
-			"<div style='margin-left: 10px;font-size: 14px;margin-top: 10px;margin-right: 10px'>产品名称："+data.product.productName+"</div>"+
-			"<div style='margin-left: 10px;font-size: 14px;margin-top: 10px;margin-right: 10px'>产品名称："+data.product.productName+"</div>";
+			"<div style='margin-left: 10px;font-size: 16px;margin-top: 10px;margin-right: 10px'>名称："+data.product.productName+"</div>"+
+			"<div style='margin-left: 10px;font-size: 16px;margin-top: 10px;margin-right: 10px'>价格："+data.product.price+"</div>"+
+			"<div style='margin-left: 10px;font-size: 16px;margin-top: 10px;margin-right: 10px'>颜色："+data.product.color+"</div>"+
+			"<div style='margin-left: 10px;font-size: 16px;margin-top: 10px;margin-right: 10px'>淘宝链接：" 
+			+"<a href='"+data.product.guideMap+"'>购买产品请点击</a></div>";
+			
+			document.getElementById("productXQ").innerHTML=
+				"<div style='margin-left: 10px;margin-top: 10px;margin-right: 10px'>"+data.product.detailInfo+"</div>";
+			var kindId=data.product.kindId;
+			var kinds=JSON.parse(localStorage.kinds);
+			
+			var categories=JSON.parse(localStorage.categories);
+			var CGId;
+			var kindName;
+			var catName;
+			var catUrl;
 			
 			
+			for(var i=0;i<kinds.length;i++){
+				
+				var kindlist=kinds[i].list;
+				for(var j=0;j<kindlist.length;j++){
+					if(kindlist[j].id==kindId){
+						 CGId=kindlist[j].categoryId;
+					     kindName=kindlist[j].kindName;
+					     kindUrl=kindlist[j].kind
+					     i=kinds.length;
+					     break;
+					}
+				}
+			}
+		
+            
+			for(var i=0;i<categories.length;i++)
+			{
+				if(categories[i].id == CGId){
+					 catUrl=categories[i].pageUrl;
+				     catName=categories[i].categoryName;
+				     break;
+				}
+			}
+			
+			document.getElementById("fuji").innerHTML=	
+		     " <a  style='font-size: 14px;' href='"+catUrl+"_C"+CGId+"'>"+catName+"</a>";
+			document.getElementById("ziji").innerHTML=	
+			     " <a  style='font-size: 14px;' href='"+catUrl+"_K"+kindId+"'>"+kindName+"</a>";
 			
 		}
 	});
